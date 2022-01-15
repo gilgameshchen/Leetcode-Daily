a851ebe027d85bfde3c9f4a7f9a5bded01ef65d6
cin <1415365181@qq.com>
2022-01-15 13:27:12 +0800

    cin finish day 18

--- auto.bat
+++ auto.bat
@@ -6,8 +6,9 @@ git fetch upstream
 git checkout master
 git merge upstream/master
 git add .
+@set /p username=input username:
 @set /p commit=input day:
-@ if defined commit (git commit -m "%commit%") else (git commit -m "%defaultcommit%")
+@ if defined commit (git commit -m "%username% finish day %commit%") else (git commit -m "%defaultcommit%")
 git push
 @echo finish
 @pause
